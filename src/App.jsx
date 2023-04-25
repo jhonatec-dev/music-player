b0f8c210a74d2787fafefee787a37f2732fdab51
jhonsreis <jhonsreis@gmail.com>
2023-04-25 13:33:57 -0300

    Iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Aqui terá um player lindo 🎵🖤</h1>
+      </div>
+    )
+  }
+}
